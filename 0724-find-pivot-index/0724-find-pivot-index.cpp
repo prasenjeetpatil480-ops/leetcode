c07cfe8489e4b822025f6e07a56573a4f64bba5b
prasenjeetpatil480-ops <prasenjeetpatil480@gmail.com>
2026-07-03 00:43:25 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.71%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftsum=0;
+        int rightsum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            rightsum+=nums[i];
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            rightsum-=nums[i];
+            if(rightsum==leftsum) return i;
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
