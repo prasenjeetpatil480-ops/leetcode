33d892d560d64a2ddb215795bffc898f307d4601
prasenjeetpatil480-ops <prasenjeetpatil480@gmail.com>
2026-07-13 21:15:31 +0530

    Time: 0 ms (100%), Space: 13.4 MB (45.03%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int top = 0;
+        int bottom = n - 1;
+        int row = -1;
+
+        while(top <= bottom){
+
+            int mid = top + (bottom - top) / 2;
+
+            if(target >= matrix[mid][0] && target <= matrix[mid][m-1]){
+                row = mid;
+                break;
+            }
+
+            if(target < matrix[mid][0]){
+                bottom = mid - 1;
+            }
+            else{
+                top = mid + 1;
+            }
+        }
+
+        if(row == -1)
+            return false;
+
+        int left = 0;
+        int right = m - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if(matrix[row][mid] == target)
+                return true;
+
+            if(matrix[row][mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return false;
+    }
+};
