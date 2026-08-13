b503c0d644652770bed162f37760fdb6802c8314
prasenjeetpatil480-ops <prasenjeetpatil480@gmail.com>
2026-08-13 22:25:17 +0530

    Time: 12 ms (41.89%), Space: 11.7 MB (97.54%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,10 +9,9 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-
-        while(fast!=NULL && fast->next!=NULL){
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast != nullptr && fast->next != nullptr){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
@@ -20,6 +19,5 @@ public:
             }
         }
         return false;
-
     }
 };
